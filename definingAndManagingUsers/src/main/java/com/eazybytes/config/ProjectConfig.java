22a9a085999ce29111d5596f82534f0ef18a7a5d
pkumar25 <pradeepkumar25aug@gmail.com>
2023-01-30 21:45:21 +0530

    defining and managing users

--- definingAndManagingUsers/src/main/java/com/eazybytes/config/ProjectConfig.java
+++ definingAndManagingUsers/src/main/java/com/eazybytes/config/ProjectConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -13,28 +12,38 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class ProjectConfig {
     @Bean
     SecurityFilterChain getSecurityFilterChainForProduction(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.csrf().disable().
+                authorizeHttpRequests()
                 .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
-                .requestMatchers("/contact", "/notices").permitAll()
+                .requestMatchers("/contact", "/notices","/register").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
     }
-
-    @Bean
+//below method is for in-memory users
+   /* @Bean
     InMemoryUserDetailsManager configureInMemoryUsers(){
 
-       InMemoryUserDetailsManager inMemoryUserDetailsManager=new InMemoryUserDetailsManager();
-       UserDetails admin=User.withUsername("admin").password("1234").authorities("admin").build();
+        InMemoryUserDetailsManager inMemoryUserDetailsManager=new InMemoryUserDetailsManager();
+        UserDetails admin=User.withUsername("admin").password("1234").authorities("admin").build();
         UserDetails user=User.withUsername("user").password("1234").authorities("read").build();
         inMemoryUserDetailsManager.createUser(admin);
         inMemoryUserDetailsManager.createUser(user);
         return inMemoryUserDetailsManager;
-    }
+    }*/
+
+    //below method is for jdbc
+//    @Bean
+//    UserDetailsManager configureJdbcUsers(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
---
+++ definingAndManagingUsers/src/main/java/com/eazybytes/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.eazybytes.controller;
+
+import com.eazybytes.model.Customer;
+import com.eazybytes.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        Customer savedCustomer = null;
+        ResponseEntity responseEntity = null;
+        try {
+            savedCustomer = repository.save(customer);
+            if (savedCustomer.getId() > 0) {
+                responseEntity = ResponseEntity.status(HttpStatus.CREATED)
+                        .body("Given user details are successfully registered");
+            }
+        } catch (Exception e) {
+            responseEntity = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occured due to : " + e.getMessage());
+        }
+        return responseEntity;
+    }
+}
